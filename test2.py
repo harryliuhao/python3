2a05a3e3b30714d1e09e0c179fe9c2ba56a388fc
harryliuhao <harryliuhao@hotmail.com>
2020-03-06 22:40:58 -0500

    first turtle

---
+++ test2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 14:35:04 2020
+
+@author: Hao
+"""
+
+import turtle
+
+paper=turtle.Screen()
+kai=turtle.Turtle()
+kai.shape("turtle")
+
+dist=3
+accelaration=0.05
+turn=20
+steps=90
+
+for _ in range(steps):
+    kai.forward(dist)
+    kai.right(turn)
+    dist=dist*(1+accelaration)
+    
+paper.exitonclick()
---
+++ wk2.py
@@ -0,0 +1,7 @@
+import turtle
+import random
+
+prod=random.random()
+print(prod)
+
+wn=turtle.Screen()
